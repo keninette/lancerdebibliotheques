9dab6480ef9253c41dab8ff3962ec1d2a0129087
kbj@junior <keninette.bloggeusedeluxe@gmail.com>
2017-03-28 14:02:54 +0200

    closest grid intersection found, need to move elements onto them now

--- js/grid_functions.js
+++ js/grid_functions.js
@@ -1,3 +1,7 @@
+/**
+ * Draw a grid inside a canva HTML object
+ * @param {Object} gridData :contains information about grid (position, cell space, ...)
+ */
 function drawGrid(gridData) {
     var c   = document.getElementById("grid");
     var ctx = c.getContext("2d");
@@ -21,26 +25,107 @@ function drawGrid(gridData) {
     ctx.stroke();
 }
 
+/**
+ * Move draggable element on closest grid intersection
+ * @param {Integer} x : cursor X position
+ * @param {Integer} y : cursor Y position
+ * @param {Object} gridData : contains information about grid (position, dimensions, cell space, ...)
+ */
 function setMagnetControl(x, y, gridData){    
     console.log(x+","+y);
     console.log(gridData.gridPosX);
     console.log(gridData.gridPosY);
     
-    
+    var newCoordinates = getClosestGridIntersection(x,y, gridData);
+    console.log(newCoordinates.x +"," +newCoordinates.y);
     
 }
 
-function getCloserGridIntersection(x, y, gridData){
-    /*var iterator    = 0;
-    var smallerX    = 0;
-    var biggerX     = 0;
+/**
+ * Get closest intersection coordinates on grid depending on cursor's position
+ * @param {Integer} x : cursor X position
+ * @param {Integer} y : cursor Y position
+ * @param {Object} gridData : contains information about grid (position, dimensions, cell space, ...)
+ * @returns {getClosestGridIntersection.coordinates}
+ */
+function getClosestGridIntersection(x, y, gridData){
+
+    var coordinates = {
+        "x": 0
+        ,"y": 0
+    };
+
+    // Get closest x and y positions of closest lines on grid
+    var xs = getClosestLines(gridData.gridPosX, x, (gridData.gridPosX + gridData.gridWidth), gridData.cellWidth);
+    var ys = getClosestLines(gridData.gridPosY, y, (gridData.gridPosY + gridData.gridHeight), gridData.cellHeight);
+
+    // Find which one of these two positions is the closest to mouse cursor
+    coordinates.x = findClosestValue(x, xs);
+    coordinates.y = findClosestValue(y, ys);
+
+    return coordinates;
+}
+
+/**
+ * Get closest grid lines (horizontal or vertical) for cursor position
+ * Will return smaller grid line position and bigger grid line position
+ * @param {Integer} currentGridLine : position of first grid line
+ * @param {Integer} currentCursorPosition :cursor position (x or y)
+ * @param {Integer} maxPosition : position of last grid line
+ * @param {Integer} lineSpace : space between two grid lines
+ * @returns {getClosestLines.positions} contains smaller and bigger line position on grid
+ */
+function getClosestLines(currentGridLine, currentCursorPosition, maxPosition, lineSpace){
     
-    // Get closer X position
-    iterator = gridData.gridPosX; // sooooooooo wrong
-    while(iterator <= (gridData.gridWidth + gridData.gridPosX)) {
+    var positions = {
+        "smallerLinePosition"   : 0
+        , "biggerLinePosition"  : 0
+    };
+    
+    while (positions.biggerLinePosition === 0 
+            && currentGridLine <= maxPosition) {
         
-        // If current i+1 position is bigger than x, then
-        if (x === gridData.)
+        // If cursor position is already on the grid, its value won't change
+        if (currentCursorPosition === currentGridLine) {
+            positions.biggerLinePosition    = currentCursorPosition;
+            positions.smallerLinePosition   = currentCursorPosition;
+        }
         
-    }*/
+        // If cursor position is bigger than currentGridLine position
+        // Set smaller position = currentGridLine
+        else if (currentCursorPosition > currentGridLine) {
+            positions.smallerLinePosition = currentGridLine;
+            
+        // If cursor position is smaller than currentGridLine position
+        // Set bigger position = currentGridLine position
+        // smaller position will already have been set
+        } else if (currentCursorPosition < currentGridLine) {
+            positions.biggerLinePosition = currentGridLine;
+        }
+        
+        // Go to next grid line
+        currentGridLine += lineSpace;
+    }
+    
+    return positions;
+}
+
+/**
+ * Find closest position to cursor between two choices
+ * @param {Integer} cursorPosition   : cursor position
+ * @param {Object} gridLinePositions : contains smaller and bigger grid line positions
+ * @returns {Integer} closest grid line position between the two 
+ */
+function findClosestValue(cursorPosition, gridLinePositions) {
+    
+    // If both values are equal, cursor is already on grid
+    if (gridLinePositions.smallerLinePosition ===  gridLinePositions.biggerLinePosition) {
+        return cursorPosition;
+    
+    // Else find which position is closer to cursorPosition
+    } else {
+        return (gridLinePositions.biggerLinePosition - cursorPosition) > (cursorPosition - gridLinePositions.smallerLinePosition) 
+                ? gridLinePositions.smallerLinePosition
+                : gridLinePositions.biggerLinePosition;
+    }
 }
